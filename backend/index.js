b85b2d207e1f24495ce821411264099073cadb94
bhaveshsinghal95182 <work.bhavesh22@gmail.com>
2025-04-05 18:39:11 +0000

    backend production command

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
 
-// app.use(cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//   }));
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }));
 
-app.use(cors())
+// app.use(cors())
 
 // Middleware
 app.use(bodyParser.json());
